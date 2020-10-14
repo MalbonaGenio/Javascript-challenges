ac0e1363bbc2e0b15058f9425bed1a6acd8cd5a4
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-14 05:04:18 +0000

    removed all the const variables as we don't need them.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const overlayDisplay = document.getElementById("overlay")
-
-const openModal = document.getElementById("open-modal")
-openModal.addEventListener("click", function(){
-    overlayDisplay.style.display = "block"
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
 })
 
-const closeModal = document.getElementById("close-modal")
-closeModal.addEventListener("click", function(){
-    overlayDisplay.style.display = "none"
+document.getElementById("close-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
 })
