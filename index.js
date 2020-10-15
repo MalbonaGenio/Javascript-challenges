73672832abec5825227e3838e4a04c8882b79b14
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-15 09:13:57 +0000

    added event listeners to the buttons. set the variables to perform the switch of images

---
+++ index.js
@@ -0,0 +1,14 @@
+const slides = document.getElementsByClassName('carousel-item')
+let slidePosition = 0
+const totalSlides = slides.length
+
+document.getElementById('carousel-button-next').addEventListener('click', toNextSlide)
+document.getElementById('carousel-button-prev').addEventListener('click', toPrevSlide)
+
+function ToNextSlide() {
+
+}
+
+function ToPrevSlide() {
+     
+}
