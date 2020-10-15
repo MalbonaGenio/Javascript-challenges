62af3680c29ad21156b1f66ff5fb156db936bbbd
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-15 01:01:24 +0000

    Use of the include method to compare the names array and the input string  on the search box. If they match or are included set dispaly to block and hide the one that don't match.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase()
-    let allNamesDOMCollection = document.getElementsByClassName("name")
-    for (let i = 0; i < allNamesDOMCollection.length; i++){
+    let allNamesDOMCollection = document.getElementsByClassName('name')
+    
+    for (let i = 0; i < allNamesDOMCollection.length; i++) {
         const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
-        console.log(currentName)
+        
+        if (currentName.includes(searchQuery)){
+            allNamesDOMCollection[i].style.display = "block"
+        }
+        else {
+            allNamesDOMCollection[i].style.display = "none"
+        }
     }
 })
